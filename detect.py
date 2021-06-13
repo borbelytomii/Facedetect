ba31413b0e4ceb8f9e4e760ebfa0784d4eaa2a2e
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2021-06-13 15:30:05 +0200

    Add files via upload

--- detect.py
+++ detect.py
@@ -2,24 +2,28 @@ import config as url
 import cv2
 import sys
 import logging as log
-import datetime as datetime
+import datetime as dt
 #from __future__ import print_function
 import argparse
+
 
 url.URL # config.py URl = 'x.x.x.x'
 
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#log.basicConfig(filename='webcam.log',level=log.INFO)
 
-
+#capture = cv2.VideoCapture(0)
 capture = cv2.VideoCapture(url.URL)
+anterior = 0
+
 
 while(True):
     # Capturing frame by frame
     ret,frame = capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    cv2.imshow('livestream',frame)
+    
 
 
     faces = faceCascade.detectMultiScale(
@@ -33,8 +37,12 @@ while(True):
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+    #if anterior != len(faces):
+    #    anterior = len(faces)
+    #    log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
-
+    cv2.imshow('livestream',frame)
     # Exiting the app
     if cv2.waitKey(1)  & 0xFF == ord('q'):
         break
